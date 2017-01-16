d5e33a138fef88aa07c8601d06412258292d2db1
Michael Coddington <michaelcoddington@gmail.com>
2017-01-16 12:17:36 -0500

    third commit prior to lesson 2

--- src/main/java/org/coddington/lesson2/Example2.java
+++ src/main/java/org/coddington/lesson2/Example2.java
@@ -5,6 +5,20 @@ package org.coddington.lesson2;
  */
 public class Example2 {
 
+    /*
+
+    Here's what BASIC looked like. Highly procedural.
+    -------------------------------------------------
+
+    10  A% = 2
+    20  B% = 3
+    30  C% = A% + B%
+    40  PRINT C%
+
+    RUN
+
+     */
+
     public static void main(String[] args) {
         int a = 2;
         int b = 4;
