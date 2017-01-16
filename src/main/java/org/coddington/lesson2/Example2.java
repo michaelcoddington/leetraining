3da2c269f3b5e9695c66d0743fcce9d410901171
Michael Coddington <michaelcoddington@gmail.com>
2017-01-16 12:50:02 -0500

    4th commit prior to lesson 2

--- src/main/java/org/coddington/lesson2/Example2.java
+++ src/main/java/org/coddington/lesson2/Example2.java
@@ -11,9 +11,13 @@ public class Example2 {
     -------------------------------------------------
 
     10  A% = 2
-    20  B% = 3
+    20  B% = 4
     30  C% = A% + B%
     40  PRINT C%
+    50  D% = 1
+    60  E% = 9
+    70  F% = D% + E%
+    80  PRINT F%
 
     RUN
 
@@ -24,6 +28,11 @@ public class Example2 {
         int b = 4;
         int c = a + b;
         System.out.println(c);
+
+        int d = 1;
+        int e = 9;
+        int f = d + e;
+        System.out.println(f);
     }
 
 }
