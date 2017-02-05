5fad0d7b458a68461ea294b67a1f3d87ae91dbfb
Michael Coddington <michaelcoddington@gmail.com>
2017-02-05 14:43:06 -0500

    Fixed FactorialTest for lesson 5

--- src/test/java/org/coddington/lesson5/FactorializerTest.java
+++ src/test/java/org/coddington/lesson5/FactorializerTest.java
@@ -1,6 +1,5 @@
 package org.coddington.lesson5;
 
-import org.coddington.lesson4.Factorializer;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -12,31 +11,31 @@ public class FactorializerTest {
 
     Factorializer f = new Factorializer();
 
-    @Test(expected = RuntimeException.class)
-    public void testFactorial0() {
+    @Test(expected = FactorialException.class)
+    public void testFactorial0() throws FactorialException {
         int fact = f.factorial(0);
     }
 
     @Test
-    public void testFactorial1() {
+    public void testFactorial1() throws FactorialException {
         int fact = f.factorial(1);
         assertEquals(1, fact);
     }
 
     @Test
-    public void testFactorial2() {
+    public void testFactorial2() throws FactorialException {
         int fact = f.factorial(2);
         assertEquals(2, fact);
     }
 
     @Test
-    public void testFactorial3() {
+    public void testFactorial3() throws FactorialException {
         int fact = f.factorial(3);
         assertEquals(6, fact);
     }
 
     @Test
-    public void testFactorial4() {
+    public void testFactorial4() throws FactorialException {
         int fact = f.factorial(4);
         assertEquals(24, fact);
     }
