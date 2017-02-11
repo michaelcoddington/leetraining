512ee49c7935f8b3f1a220d86a399a6f38605e29
Michael Coddington <michaelcoddington@gmail.com>
2017-02-11 12:04:59 -0500

    Added notes for lesson 6 and part of lesson 7

---
+++ src/main/java/org/coddington/lesson6/Example6.java
@@ -0,0 +1,72 @@
+package org.coddington.lesson6;
+
+/**
+ * Lesson 6: Writing data to a file
+ *
+ * @author michaelcoddington
+ */
+public class Example6 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+ * NOTES
+ *
+ * 1) public static void main(String[] args) is a special method; it is the starting point of a Java application.
+ *    when this method finishes, the application stops.
+ *
+ * 2) java.io: a package in the Java Standard Library.  It contains classes that are devoted to reading and writing
+ *    data.
+ *
+ * 3) System.out.println
+ *    "System" is a class in the Java Standard Library.  It is in the java.lang package (we'll talk about packages later).
+ *    "out" is a MEMBER of the System class (we'll talk about this in lesson 7).
+ *    It's something you can use to make messages appear on the screen (and much more)
+ *    "println" is a method that writes a string and a "newline" character (like hitting the Enter key)
+ *
+ * 4) File: a class in the java.io package. It's a class that provides access to a file on disk.
+ *
+ * 5) PrintWriter: a class in the java.io package. It's one class (there are others) that let you write data to a File.
+ *
+ * 6) Checked exception: an exception that a method says it might throw.  Anything calling that method MUST be
+ *    prepared to deal with this exception in case it is thrown by the method.
+ *
+ *    e.g.: public int divide(int numerator, int denominator) throws DivideByZeroException
+ *
+ *    in this case, DivideByZeroException is a CHECKED exception. Anything trying to call the divide method needs
+ *    to define how it will deal with this exception if it is thrown. (note: there is an alternative, discussed here --
+ *    https://docs.oracle.com/javase/tutorial/essential/exceptions/declaring.html -- but we'll cover this later).
+ *
+ * 7) try/catch/finally: a control structure in Java that lets you specify how an application should cope with an
+ *    Exception.  e.g. TRY to do this.  If an exception is THROWN, then CATCH it and do xyz.  And no matter what,
+ *    FINALLY do blahblahblah when the try or catch is finished working.
+ *
+ * 8) CSV: a "comma-separated value" file.  Excel understands and can import this kind of file.  It contains lines like:
+ *    1,"a"
+ *    2,"b"
+ *    3,"c"
+ *
+ *    or
+ *
+ *    1,10,6
+ *    4,8,165
+ *    1094,5,0
+ *
+ *    All the lines should have the same number of values.  These will be translated into columns in Excel.
+ *
+ * 9) String concatenation:
+ *    You can use the + operator with String objects.  For example, "bad" + "ass" is an expression that returns a
+ *    new string "badass".  This is called string concatenation.  You can also concatenate strings and numbers, e.g.
+ *    "hey" + 420 is an expression that returns a new string "hey420".
+ *
+ *    You can concatenate multiple strings and numbers, so "hi" + 3 + "there" + 42 is an expression that returns a new
+ *    string "hi3there42".
+ *
+ * 10) Imports:
+ *    When a class in one package needs to use a class in a different package, it needs to "import" that class using
+ *    an import statement.  This lets Java know precisely which class is being used, as there may be classes with the same
+ *    name but in different packages, so Java needs to know exactly which class you're using.
+ */
---
+++ src/main/java/org/coddington/lesson7/Example7.java
@@ -0,0 +1,29 @@
+package org.coddington.lesson7;
+
+/**
+ * @author michaelcoddington
+ */
+public class Example7 {
+}
+
+/*
+ * NOTES
+ *
+ * 1) INTERFACE: a contract that defines a set of capabilities.  A class is said to IMPLEMENT an interface
+ *    by (a) declaring that it implements the interface and (b) by defining methods whose signature, modifiers,
+ *    and return types match those defined in the interface.
+ *
+ * 2) METHOD SIGNATURE: a combination of the method's name and its parameter types.
+ *    e.g.: a method public int add(int a, int b) has the signature add(int, int)
+ *
+ * 3) MODIFIERS: keywords that provide additional information about a method or class
+ *    e.g.: in a method like public static void main(String[] args), "public" and "static" are modifiers.
+ *    "public" indicates this method can be called by any object.
+ *    "static" indicates this method is not associated with a specific object, but with the class itself
+ *       (we'll discuss this later)
+ *
+ * 4) MEMBER:
+ *    A variable associated with an object.
+ *
+ *
+ */
