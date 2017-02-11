be465fd2d4a0f3b113f647c47c5b414970a2150d
Michael Coddington <michaelcoddington@gmail.com>
2017-02-11 18:32:06 -0500

    Added challenge file

---
+++ src/main/java/org/coddington/lesson6/Challenge6.java
@@ -0,0 +1,18 @@
+package org.coddington.lesson6;
+
+/**
+ * @author michaelcoddington
+ */
+public class Challenge6 {
+
+    /*
+     * Challenge: create a file called factorial.csv.  Using the Factorializer from lesson 5,
+     * write a file that contains two values per line: a number in column 1, and its factorial in column 2,
+     * for all numbers between 1 and 10.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
