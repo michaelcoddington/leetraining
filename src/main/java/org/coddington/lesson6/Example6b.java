8cc832671092796a73dedb052345569d6e9253ba
Michael Coddington <michaelcoddington@gmail.com>
2017-02-23 20:02:47 -0500

    Second example for lesson 6

---
+++ src/main/java/org/coddington/lesson6/Example6b.java
@@ -0,0 +1,32 @@
+package org.coddington.lesson6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ * Loop-writing!
+ *
+ * @author michaelcoddington
+ */
+public class Example6b {
+
+    public static void main(String[] args) {
+
+        File f = new File("test.txt");
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(f);
+            for (int i = 0; i < 5; i += 1) {
+                pw.println("i is " + i);
+            }
+        } catch (FileNotFoundException nfne) {
+            System.out.println("oh well.");
+        } finally {
+            if (pw != null) {
+                pw.close();
+            }
+        }
+
+    }
+}
